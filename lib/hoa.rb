98405db4f6796d5b191bf89323daf9302097b582
Cindy Kei <myk245@nyu.edu>
2019-11-20 03:21:40 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,7 +6,8 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  add_character(:third_earthers, "Snarf") = {
+  add_character(:third_earthers, "Snarf") 
+  {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
