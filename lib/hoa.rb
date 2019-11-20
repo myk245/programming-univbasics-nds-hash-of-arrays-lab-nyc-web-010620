1a4aeb3b92041cd5432413257f4c14928c64cfdf
Cindy Kei <myk245@nyu.edu>
2019-11-20 03:16:14 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -11,7 +11,8 @@ def add_character(show, name)
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 } 
-  return BASE_HOA[:third_earthers]
+  add_character[:third_earthers] << "Snarf"
+  
   # Write your implementation here
   # Should return the array of the 'show' argument
 end
